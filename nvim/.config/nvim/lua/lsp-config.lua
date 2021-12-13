965cac4b95420467e64ae54c7e412888df97ff1a
Praveen Perumal <ppraveen98841@gmail.com>
2021-12-13 09:28:44 +0530

    Simplified the lsp-config process

--- nvim/.config/nvim/lua/lsp-config.lua
+++ nvim/.config/nvim/lua/lsp-config.lua
@@ -1,11 +1,20 @@
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities.textDocument.completion.completionItem.snippetSupport = true
+local EnableSnippet = vim.lsp.protocol.make_client_capabilities()
+EnableSnippet.textDocument.completion.completionItem.snippetSupport = true
 
-local nvim_lsp = require'lspconfig'
-local servers = {'clangd','html','pyright'}
+local server = {
+    'clangd',
+    'pyright',
+    'html',
+    'cssls',
+    'tsserver'
+    -- For more server.. (check https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md)
+}
 
-for _,lsp in ipairs(servers) do
-    nvim_lsp[lsp].setup{
-        capabilities = capabilities,
+for key,value in ipairs(server) do
+    require'lspconfig'[value].setup{
+        capabilities = EnableSnippet,
+        root_dir = function()
+            return vim.loop.cwd()
+        end
     }
 end
