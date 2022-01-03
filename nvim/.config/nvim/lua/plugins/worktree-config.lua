b2b786c0565817950991d3a810eb6c9093637505
Praveen Perumal <ppraveen98841@gmail.com>
2022-01-03 08:38:53 +0530

    Changed :Ex -> :Alpha

--- nvim/.config/nvim/lua/plugins/worktree-config.lua
+++ nvim/.config/nvim/lua/plugins/worktree-config.lua
@@ -2,5 +2,5 @@ local Worktree = require("git-worktree")
 
 Worktree.on_tree_change(function(op,metadata)
     vim.api.nvim_command('bufdo bdelete')
-    vim.api.nvim_command('Ex')
+    vim.api.nvim_command('Alpha')
 end)
