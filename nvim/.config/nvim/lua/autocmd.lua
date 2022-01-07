13485b89144630b581a83ca41a230a0de241aca1
Praveen Perumal <ppraveen98841@gmail.com>
2022-01-07 19:00:51 +0530

    Update autocmd to use lua function instead

--- nvim/.config/nvim/lua/autocmd.lua
+++ nvim/.config/nvim/lua/autocmd.lua
@@ -1,53 +1,35 @@
--- Remove Trailing Spaces
-vim.cmd[[
-aug RemoveSpaces
-  au!
-  au BufWritePre * :%s/\s\+$//e
-aug END
-]]
+local create_augroups = require("utils").nvim_create_augroups
 
--- Reloading Snippets after Editing
-vim.cmd[[
-aug ReloadSnippets
-  au!
-  au BufWritePost *.snippets :CmpUltisnipsReloadSnippets
-aug END
-]]
+local autocmds = {
+  rm_space = {
+    { 'BufWritePre','*',':%s/\\s\\+$//e' },
+  };
 
--- Set compiler
-vim.cmd[[
-aug SetCompiler
-  au!
-  au BufEnter *.cpp :compiler clang
-  au BufEnter *.ts :compiler tsc
-aug END
-]]
+  reload_snippets = {
+    { 'BufWritePost','*.snippets',':CmpUltisnipsReloadSnippets' },
+  };
 
--- Enable spell
-vim.cmd[[
-aug EnableSpell
-  au!
-  au BufEnter *.md :setlocal spell spelllang=en_us
-aug END
-]]
+  set_compiler = {
+    { 'BufEnter','*.cpp',':compiler clang' },
+    { 'BufEnter','*.ts',':compiler tsc' },
+  };
 
--- Set comment string
-vim.cmd[[
-aug SetCommets
-  au!
-  au BufEnter *.cpp :setlocal commentstring=//\ %s
-aug END
-]]
+  set_spell = {
+    { 'BufEnter','*.md',':setlocal spell spelllang=en_us' },
+  };
 
--- Set <leader>x
-vim.cmd[[
-aug ExecuteCommand
-  au!
-  au BufEnter * nnoremap <leader>x :echo "Set a Execute command in autocmd.vim"<CR>
-  au BufEnter *.cpp nnoremap <leader>x :FloatermNew ./%:t:r.out<CR>
-  au BufEnter *.ts nnoremap <leader>x :FloatermNew ts-node %<CR>
-  au BufEnter *.js nnoremap <leader>x :FloatermNew node %<CR>
-  au BufEnter *.py nnoremap <leader>x : FloatermNew python3 %<CR>
-  au BufEnter *.wiki nnoremap <leader>x :!~/.config/nvim/wiki2pdf/wiki2pdf % ~/Documents/Notes/Add_to/%:t:r.pdf<CR>
-aug END
-]]
+  set_comment_str = {
+    { 'BufEnter','*.cpp',':setlocal commentstring=//\\ %s' },
+  };
+
+  set_execute = {
+    { 'BufEnter','*','nnoremap <leader>x :echo "Set a Execute command in autocmd.vim"<CR>' },
+    { 'BufEnter','*.cpp','nnoremap <leader>x :FloatermNew ./%:t:r.out<CR>' },
+    { 'BufEnter','*.ts','nnoremap <leader>x :FloatermNew ts-node %<CR>' },
+    { 'BufEnter','*.js','nnoremap <leader>x :FloatermNew node %<CR>' },
+    { 'BufEnter','*.py','nnoremap <leader>x : FloatermNew python3 %<CR>' },
+    { 'BufEnter','*.wiki','nnoremap <leader>x :!~/.config/nvim/wiki2pdf/wiki2pdf % ~/Documents/Notes/Add_to/%:t:r.pdf<CR>' },
+  };
+}
+
+create_augroups(autocmds);
