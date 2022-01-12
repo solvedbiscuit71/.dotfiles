5b916b8e70338ca8568125330e99e72fcafdc882
Praveen Perumal <ppraveen98841@gmail.com>
2022-01-12 08:25:06 +0530

    nvim: Removed 'Ex' callback from worktree
    As, LuaTree disables :Ex

--- nvim/.config/nvim/lua/plugin-config/worktree.lua
+++ nvim/.config/nvim/lua/plugin-config/worktree.lua
@@ -3,5 +3,4 @@ local Worktree = require("git-worktree")
 Worktree.on_tree_change(function(op,metadata)
     -- callback
     vim.api.nvim_command('bufdo bdelete')
-    vim.api.nvim_command('Ex')
 end)
