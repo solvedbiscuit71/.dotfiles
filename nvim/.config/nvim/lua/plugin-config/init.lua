bb5387dba608891782b3e27342ee85c326fce036
Praveen Perumal <ppraveen98841@gmail.com>
2022-01-19 07:09:54 +0530

    nvim: Commented treesitter configuration
    Since, Install all the TS module at once can be problematic.

--- nvim/.config/nvim/lua/plugin-config/init.lua
+++ nvim/.config/nvim/lua/plugin-config/init.lua
@@ -3,8 +3,10 @@ require'plugin-config.cmp'
 require'plugin-config.gitsigns'
 require'plugin-config.lsp'
 require'plugin-config.telescope'
-require'plugin-config.treesitter'
 require'plugin-config.worktree'
 require'plugin-config.lualine'
 require'plugin-config.bufferline'
 require'plugin-config.luatree'
+
+-- Uncomment the below line to install all the TS modules
+-- require'plugin-config.treesitter'
